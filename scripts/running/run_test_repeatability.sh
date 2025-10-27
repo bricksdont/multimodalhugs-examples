d3fb8811ebf6d7df6782b1ba44ed02cd37ae8379
bricksdont <mathias.mueller@uzh.ch>
2025-10-27 13:22:52 +0100

    test repeatability

---
+++ scripts/running/run_test_repeatability.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+base="/shares/sigma.ebling.cl.uzh/mathmu/multimodalhugs-examples"
+scripts=$base/scripts
+
+# set to "false" or "true":
+
+dry_run="false"
+
+model_name="phoenix"
+
+# best hyperparams found so far
+
+learning_rate="1e-5"
+warmup_steps=500
+label_smoothing_factor="0.0"
+gradient_accumulation_steps=3
+
+. $scripts/running/run_generic.sh
